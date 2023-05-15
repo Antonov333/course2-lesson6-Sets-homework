7e171dad2e58320054ba23d831735736712ed772
Antonov333 <antonovsergei333@yahoo.com>
2023-05-15 22:32:27 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course2lesson6setshomework/Course2Lesson6SetsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2lesson6setshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lesson6SetsHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lesson6SetsHomeworkApplication.class, args);
+    }
+
+}
